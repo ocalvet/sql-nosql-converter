3c4f56038b642b115351a7bcee896c2538807fff
Ovidio R. Calvet <calveto@gmail.com>
2016-05-06 09:57:27 -0400

    Updated the system to go and get a user data

--- app/SQLConnector.js
+++ app/SQLConnector.js
@@ -1,6 +1,27 @@
+var mysql = require("mysql");
+
 class SQLConnector {
-  constructor(conf) {
-    console.log('configuration', config);
+  constructor(config) {
+    this.con = mysql.createConnection({
+      host: config.host,
+      user: config.user,
+      password: config.password,
+      database: config.database
+    });
+  }
+  
+  getUser(id) {
+    return new Promise((resolve, reject) => {
+      this.con.query('SELECT * FROM user where userId = ?', [id], (err, rows) => {
+        this.con.end();
+        if(err) {
+          reject(err);
+          return;
+        };
+        console.log('Data received from Db:\n', rows);
+        resolve(rows[0]);
+      });
+    })
   }
 }
 
--- app/SQLToNoSQLConverter.js
+++ app/SQLToNoSQLConverter.js
@@ -1,6 +1,11 @@
+let SQLConnector = require('./SQLConnector');
+
 class SQLToNoSQLConverter {
   constructor(config) {
     console.log('Converter created', config);
+    this.sqlConnector = new SQLConnector(config.scoutlitSQL);
+    this.sqlConnector.getUser(47)
+      .then((user) => console.log('User data', user.FirstName), (err) => console.log('error:', err));
   }
 }
 
