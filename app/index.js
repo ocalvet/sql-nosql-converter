1aa32942fbb45a11dd714004917486c116c0b33f
Ovidio R. Calvet <calveto@gmail.com>
2016-05-06 09:12:01 -0400

    Added a converter and a configuration file

---
+++ app/index.js
@@ -0,0 +1,7 @@
+class SQLToNoSQLConverter {
+  constructor(config) {
+    console.log('Converter created', config);
+  }
+}
+
+module.exports = SQLToNoSQLConverter;
---
+++ index.js
@@ -0,0 +1,4 @@
+let SQLToNoSQLConverter = require('./app/index');
+let config = require("./config");
+
+let application = new SQLToNoSQLConverter(config);
